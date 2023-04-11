bea433ac40120e29e61506a3b2656d791949bf12
Raymond Amenah <raymondamenah@gmail.com>
2023-04-11 23:21:25 +0100

    reset password

--- src/Components/Authentication/AuthPage.jsx
+++ src/Components/Authentication/AuthPage.jsx
@@ -75,157 +75,163 @@ const AuthPage = () => {
 
   const sendResetPassword = async (e) => {
     e.preventDefault();
-    await sendPasswordResetEmail(form.email);
-    toast.success('reset password email sent');
-  };
+    try {
+      await sendPasswordResetEmail(form.email);
+      toast.success('reset password email sent please check your inbox');
+    } catch (error) {
+      toast.error('this email is not registered');
 
-  const Heading = () => {
-    const date = new Date();
-    if (date.getHours() < 12) {
-      return "Good Morning";
-    } else if (date.getHours() > 12 && date.getHours() < 18) {
-      return "Good Afternoon";
-    } else {
-      return "Good Evening";
-    }
-  };
+    };
 
-
-  return (
-    <section className='auth-section-container'>
-      <div className='auth-container'>
-        <div className='auth-image-container'>
-          <Image
-            className='auth-image'
-            src={form.isNewUser ? '/signUpImage.webp' : '/signInImage.webp'}
-            width='1'
-            height='1'
-            alt='auth image'
-          />
-        </div>
+    const Heading = () => {
+      const date = new Date();
+      if (date.getHours() < 12) {
+        return "Good Morning";
+      } else if (date.getHours() > 12 && date.getHours() < 18) {
+        return "Good Afternoon";
+      } else {
+        return "Good Evening";
+      }
+    };
 
 
-        <section className='auth-section'>
-          <h1>{Heading()}</h1>
-          <h5>{!form.isNewUser ? 'Welcome Back! Enter your details to sign in.' : ' Welcome! Enter your details to sign up.'}</h5>
+    return (
+      <section className='auth-section-container'>
+        <div className='auth-container'>
+          <div className='auth-image-container'>
+            <img
+              className='auth-image'
+              src={form.isNewUser ? '/signUpImage.webp' : '/signInImage.webp'}
+
+              alt='auth image'
+            />
+          </div>
+
+
+          <section className='auth-section'>
+            <h1>{Heading()}</h1>
+            {!form.forgotPassword &&
+              <h5>
+                {!form.isNewUser ? 'Welcome Back! Enter your details to sign in.' : ' Welcome! Enter your details to sign up.'}
+              </h5>
+            }
+            <form className='form' onSubmit={handleSubmit}>
+
+
+              {/* if its a new user and he doesnt forget his password this shows up */}
+
+
+              {(!form.forgotPassword) &&
+                <>
+                  <label htmlFor='email'>Email</label>
+                  <input
+                    className='auth-input'
+                    type='email'
+                    name='email'
+                    value={form.email}
+                    placeholder='Email'
+                    required
+                    onChange={onChange}
+                  />
+
+                  <label htmlFor='password'>Password</label>
+                  <input
+                    className='auth-input'
+                    type='password'
+                    name='password'
+                    placeholder='Password'
+                    required
+                    value={form.password}
+                    onChange={onChange}
+                  />
 
 
 
-          <form className='form' onSubmit={handleSubmit}>
+                  {/* confirm password only shows if the person is creating an account, it is hidden when signing in  */}
 
 
-            {/* if its a new user and he doesnt forget his password this shows up */}
+                  {form.isNewUser &&
+                    <>
+                      <label htmlFor='confirm password'>Confirm Password</label>
+                      <input
+                        className='auth-input'
+                        type='password'
+                        name='confirmPassword'
+                        placeholder='Password'
+                        required
+                        value={form.confirmPassword}
+                        onChange={onChange}
+                      />
+                    </>}
+
+
+                  {/* forgot password option only shows if the person is signing in  */}
+
+
+                  {!form.isNewUser && <div className='forgot-password' onClick={() => setForm((prev) => ({
+                    ...prev,
+                    forgotPassword: !prev.forgotPassword
+                  }))}>
+                    Forgot Password?
+                  </div>
+                  }
+
+                  <div className='auth-group-buttons'>
+                    <input
+                      className='submit-button'
+                      type='submit'
+                      value={form.isNewUser ? 'Sign Up' : 'Log In'}
+                    />
+                    <p className='or'>OR</p>
+                    <button
+                      className='google-button'
+                      onClick={googleSignIn}>Continue with Google</button>
+                  </div>
+                </>
+              }
+
+              {/* this is the reset password section it will only display if the user forgets his password and if it is not a new user */}
+
+
+              {(form.forgotPassword && !form.isNewUser) &&
+                <>
+                  <label htmlFor='email'>Email</label>
+                  <input
+                    className='auth-input'
+                    type='email'
+                    name='email'
+                    value={form.email}
+                    placeholder='Email'
+                    required
+                    onChange={onChange}
+                  />
+                  <div className='forgot-password' onClick={() => setForm((prev) => ({
+                    ...prev,
+                    forgotPassword: !prev.forgotPassword
+                  }))}>Log In</div>
+                  <button className='reset-password' onClick={sendResetPassword}>Reset Password</button>
+                </>}
+            </form>
+
+
+            {/* conditional option displays only when SignIn/SignUp */}
 
 
             {(!form.forgotPassword) &&
-              <>
-                <label htmlFor='email'>Email</label>
-                <input
-                  className='auth-input'
-                  type='email'
-                  name='email'
-                  value={form.email}
-                  placeholder='Email'
-                  required
-                  onChange={onChange}
-                />
-
-                <label htmlFor='password'>Password</label>
-                <input
-                  className='auth-input'
-                  type='password'
-                  name='password'
-                  placeholder='Password'
-                  required
-                  value={form.password}
-                  onChange={onChange}
-                />
-
-
-
-                {/* confirm password only shows if the person is creating an account, it is hidden when signing in  */}
-
-
-                {form.isNewUser &&
-                  <>
-                    <label htmlFor='confirm password'>Confirm Password</label>
-                    <input
-                      className='auth-input'
-                      type='password'
-                      name='confirmPassword'
-                      placeholder='Password'
-                      required
-                      value={form.confirmPassword}
-                      onChange={onChange}
-                    />
-                  </>}
-
-
-                {/* forgot password option only shows if the person is signing in  */}
-
-
-                {!form.isNewUser && <div className='forgot-password' onClick={() => setForm((prev) => ({
-                  ...prev,
-                  forgotPassword: !prev.forgotPassword
-                }))}>Forgot Password?</div>
-                }
-
-                <div className='auth-group-buttons'>
-                  <input
-                    className='submit-button'
-                    type='submit'
-                    value={form.isNewUser ? 'Sign Up' : 'Log In'}
-                  />
-                  <p className='or'>OR</p>
-                  <button
-                    className='google-button'
-                    onClick={googleSignIn}>Continue with Google</button>
-                </div>
-              </>
+              <div className='no-account'>{form.isNewUser ? 'Have an account?' : 'No account?'}
+                <span className='no-account-button' onClick={() =>
+                  setForm((prev) => ({
+                    ...prev,
+                    isNewUser: !prev.isNewUser,
+                  }))}>
+                  {form.isNewUser ? 'Log In' : 'Sign Up'}
+                </span>
+              </div>
             }
-
-            {/* this is the reset password section it will only display if the user forgets his password and if it is not a new user */}
-
-
-            {(form.forgotPassword && !form.isNewUser) &&
-              <>
-                <label htmlFor='email'>Email</label>
-                <input
-                  className='auth-input'
-                  type='email'
-                  name='email'
-                  value={form.email}
-                  placeholder='Email'
-                  required
-                  onChange={onChange}
-                />
-                <div className='forgot-password' onClick={() => setForm((prev) => ({
-                  ...prev,
-                  forgotPassword: !prev.forgotPassword
-                }))}>Log In</div>
-                <button className='reset-password' onClick={sendResetPassword}>Reset Password</button>
-              </>}
-          </form>
-
-
-          {/* conditional option displays only when SignIn/SignUp */}
-
-
-          {(!form.forgotPassword) &&
-            <div className='no-account'>{form.isNewUser ? 'Have an account?' : 'No account?'}
-              <span className='no-account-button' onClick={() =>
-                setForm((prev) => ({
-                  ...prev,
-                  isNewUser: !prev.isNewUser,
-                }))}>
-                {form.isNewUser ? 'Log In' : 'Sign Up'}
-              </span>
-            </div>
-          }
-        </section>
-      </div>
-    </section >
-  );
+          </section>
+        </div>
+      </section >
+    );
+  };
 };
-
 export default AuthPage;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,11 @@
-// import AuthContext from "../Context/AuthContext";
+import AuthContext from "../Context/AuthContext";
 import "@/styles/globals.css";
 import "../Components/Authentication/style.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <AuthContext>
+      <Component {...pageProps} />;
+    </AuthContext>
+  );
 }
