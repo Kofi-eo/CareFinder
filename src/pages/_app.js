0e23c51bdaaa11feb9ff312908099341a5aed6b0
caleb <calebakpan7@gmail.com>
2023-04-14 19:09:12 +0100

    commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,14 +3,14 @@ import "@/styles/globals.css";
 import "@/styles/style.css";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import AuthContext from "../Context/AuthContext";
+// import AuthContext from "../Context/AuthContext";
 import { MapsContextProvider } from "@/Context/googleMapsContext";
 
 
 export default function App({ Component, pageProps }) {
   
   return (
-    <AuthContext>
+    // <AuthContext>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <MapsContextProvider>  
           <Layout>
@@ -18,6 +18,6 @@ export default function App({ Component, pageProps }) {
           </Layout>
         </MapsContextProvider>
       </LocalizationProvider>
-    </AuthContext>
+    // </AuthContext>
   );
 }
