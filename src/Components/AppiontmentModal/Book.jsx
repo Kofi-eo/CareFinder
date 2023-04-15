08c66858fdf75dd02554145478e3866704926ef6
ITACHI1117 <ajogujoseph0317@gmail.com>
2023-04-15 13:58:13 +0100

    done with the booking modal

--- src/Components/AppiontmentModal/Book.jsx
+++ src/Components/AppiontmentModal/Book.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import PopUpModal from "@/styles/PopUpModal.module.css";
 import { RiHospitalLine } from "react-icons/ri";
+import { MdAccessTime, MdOutlineMeetingRoom } from "react-icons/md";
 
 function Book() {
   return (
     <>
+      {/* Doctor Details */}
       <div className={PopUpModal.AppointmentDetails}>
         <h1>Appointment Details</h1>
         <div className={PopUpModal.drDetails}>
@@ -18,6 +20,42 @@ function Book() {
             </p>
           </div>
         </div>
+      </div>
+      {/* Date and Time  */}
+      <div className={PopUpModal.AppointmentTime}>
+        <div className={PopUpModal.AppointmentTimeText}>
+          <p>
+            <MdAccessTime
+              style={{
+                color: "black",
+                marginTop: "-22px",
+                fontSize: "25px",
+                marginRight: "5px",
+              }}
+            />{" "}
+            Today, 12 Mar 2023 <br />
+            7:15 AM
+          </p>
+          <p>
+            <MdOutlineMeetingRoom
+              style={{ color: "black", fontSize: "25px", marginRight: "5px" }}
+            />{" "}
+            In Person
+          </p>
+        </div>
+      </div>
+      {/* Patient Email and Contact */}
+      <div className={PopUpModal.patientDetails}>
+        <form action="">
+          <div className={PopUpModal.patientInput}>
+            <label htmlFor="Email">Email</label>
+            <input type="text" placeholder="testingMic12@gmail.com" />
+          </div>
+          <div className={PopUpModal.patientInput}>
+            <label htmlFor="Email">Phone</label>
+            <input type="text" placeholder="+234 0513564" />
+          </div>
+        </form>
       </div>
     </>
   );
