f2c448347acb963ce5f0cd31d8ec86ba878ca61e
Emmanuel Ofori <slxmtube@gmail.com>
2023-04-18 23:12:12 +0000

    booking modal

---
+++ src/components/AppointmentModal/MeetingTime.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { RiArrowLeftLine, RiArrowRightLine } from "react-icons/ri";
+import PopUpModal from "@/styles/PopUpModal.module.css";
+import MeetingTimeCarousel from "./MeetingTimeCarousel.jsx";
+
+function MeetingTime() {
+  return (
+    <>
+      <div className={PopUpModal.meetingTime}>
+        <div className={PopUpModal.ArrBtns}>
+          <div className="image6-swiper-button-prev">
+            <RiArrowLeftLine />
+          </div>
+          <div className="image6-swiper-button-next">
+            <RiArrowRightLine />
+          </div>
+        </div>
+        <MeetingTimeCarousel />
+      </div>
+    </>
+  );
+}
+
+export default MeetingTime;
