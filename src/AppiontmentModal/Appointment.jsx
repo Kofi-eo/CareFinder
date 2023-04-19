ec53b7b64e2ea211788bd3718d5f812cf0f9ec66
Emmanuel Ofori <slxmtube@gmail.com>
2023-04-19 00:33:51 +0000

    PopUp Modal

---
+++ src/AppiontmentModal/Appointment.jsx
@@ -0,0 +1,36 @@
+import PopUpModal from "@/styles/PopUpModal.module.css";
+import { color } from "framer-motion";
+import { useState } from "react";
+import { AiFillStar } from "react-icons/ai";
+import { MdHeight } from "react-icons/md";
+import { RiHospitalLine } from "react-icons/ri";
+import MeetingOption from "./MeetingOption";
+import MeetingDate from "./MeetingDate";
+import MeetingTime from "./MeetingTime";
+
+function Appointment() {
+  return (
+    <>
+      <div className={PopUpModal.appointmentContainer}>
+        <div className={PopUpModal.doctorImg}>
+          <img src="/DrOwen.png" alt="" />
+        </div>
+        <div className={PopUpModal.doctordetails}>
+          <h1>Dr. Paul Eneche</h1>
+          <h2>Dentist</h2>
+          <p>
+            <AiFillStar style={{ color: "#FFD75F" }} /> 4.5 (54)
+          </p>
+          <p>
+            <RiHospitalLine /> Elina Hospital
+          </p>
+        </div>
+      </div>
+      <MeetingOption />
+      <MeetingDate />
+      <MeetingTime />
+    </>
+  );
+}
+
+export default Appointment;
