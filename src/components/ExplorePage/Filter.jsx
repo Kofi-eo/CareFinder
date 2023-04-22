2c97a04b290a9f7f0c2f8144e7cd302b9937bac4
Emmanuel Ofori <slxmtube@gmail.com>
2023-04-22 20:44:41 +0000

    filter update

--- src/components/ExplorePage/Filter.jsx
+++ src/components/ExplorePage/Filter.jsx
@@ -3,6 +3,8 @@ import ExploreStyles from '@/styles/ExplorePage.module.css';
 import { GoSearch } from 'react-icons/go';
 import { RiArrowDownSLine } from 'react-icons/ri';
 import { GoCalendar } from 'react-icons/go';
+import { departmentsData } from '@/data/departmentsData';
+import { StateData } from '@/data/locationData';
 
 export default function Filter() {
 	const xfilter = () => {
@@ -12,13 +14,35 @@ export default function Filter() {
 		//
 		<div className='filter-container'>
 			<form onSubmit={xfilter} className='filter-form'>
-				<input type='text' placeholder='City' />
-				<input type='text' placeholder='State' />
+				<input type='text' placeholder='Area' />
+				<select placeholder='State'>
+					<option value='text' data-placeholder='State 1'>
+						Select A State
+					</option>
+					{StateData.map((x, index) => (
+						<option
+							value={x}
+							data-placeholder={`State ${index + 1}`}
+							key={index}
+						>
+							{x}
+						</option>
+					))}
+				</select>
 
 				<select placeholder='Department'>
 					<option value='text' data-placeholder='Department 1'>
 						Select A Department
 					</option>
+					{departmentsData.map((x, index) => (
+						<option
+							value={x}
+							data-placeholder={`Department ${index + 1}`}
+							key={index}
+						>
+							{x}
+						</option>
+					))}
 				</select>
 
 				<input type='datetime-local' />
---
+++ src/data/departmentsData.js
@@ -0,0 +1,17 @@
+export const departmentsData = [
+	'Cardiology',
+	'Pediatrics',
+	'Dermatology',
+	'Gastroenterology',
+	'Ophthalmology',
+	'Neurology',
+	'Obstetrics & Gynecology',
+	'Oncology',
+	'Psychiatry',
+	'Orthopedics',
+	'Dentistry',
+	'Rheumatology',
+	'ENT',
+	'Nephrology',
+	'Endocrinology',
+];
