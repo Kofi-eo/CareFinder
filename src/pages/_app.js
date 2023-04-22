cb763aab70d4d567aaa06c9c15873d99c1197117
Emmanuel Ofori <slxmtube@gmail.com>
2023-04-22 20:10:04 +0000

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,17 +5,44 @@ import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 // // import AuthContext from "../Context/AuthContext";
 import { MapsContextProvider } from '@/Context/googleMapsContext';
+import { onAuthStateChanged } from 'firebase/auth';
+import { createContext, useContext, useEffect, useState } from 'react';
+import { auth } from '../Firebase/firebase.config';
+
+const Auth = createContext();
 
 export default function App({ Component, pageProps }) {
+	const [signedIn, setSignedIn] = useState(false);
+	const [user, setUser] = useState(null);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		setLoading(true);
+
+		onAuthStateChanged(auth, async (user) => {
+			if (user) {
+				setUser(user);
+			}
+			setLoading(false);
+			setSignedIn(!!user);
+		});
+	}, []);
+
 	return (
-		// // <AuthContext>
-		<LocalizationProvider dateAdapter={AdapterDayjs}>
-			<MapsContextProvider>
-				<Layout>
-					<Component {...pageProps} />
-				</Layout>
-			</MapsContextProvider>
-		</LocalizationProvider>
-		// // </AuthContext>
+		<Auth.Provider
+			value={{ user, signedIn, loading, setSignedIn, setUser, setLoading }}
+		>
+			<LocalizationProvider dateAdapter={AdapterDayjs}>
+				<MapsContextProvider>
+					<Layout>
+						<Component {...pageProps} />
+					</Layout>
+				</MapsContextProvider>
+			</LocalizationProvider>
+		</Auth.Provider>
 	);
 }
+
+export const useAuth = () => {
+	return useContext(Auth);
+};
