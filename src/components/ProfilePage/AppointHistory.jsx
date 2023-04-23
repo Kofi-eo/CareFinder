f80de9f385d51646dece00428b806d2c9d9983bf
caleb <calebakpan7@gmail.com>
2023-04-23 20:06:47 +0100

    bug fixes

--- src/components/ProfilePage/AppointHistory.jsx
+++ src/components/ProfilePage/AppointHistory.jsx
@@ -2,12 +2,12 @@ import { PT_Sans } from "next/font/google";
 
 const pt_sans = PT_Sans({ subsets: ["latin"], weight: ["400", "700"] });
 
-export default function AppointmentHistory() {
+export default function AppointmentHistory({history}) {
     console.log(history)
     const display = history.length <= 0 ?
         <p>You currently have no appointments</p> :
-        history.map(({meetingTime, meetingDate, sex, firstName, lastName, appointmentType}) => (
-            <div style={{border: '1px solid green', marginTop: '1em', padding: '0.6em', borderRadius: '10px'}}>
+        history.map(({meetingTime, meetingDate, sex, firstName, lastName, appointmentType}, i) => (
+            <div key={i} style={{border: '1px solid green', marginTop: '1em', padding: '0.6em', borderRadius: '10px'}}>
                 <p><span style={{color: 'green'}}>Appointment with </span>- Dr Paul Eneche @{meetingTime} on {meetingDate}</p>
                 <p><span style={{color: 'green'}}>Appointment Type </span>- {appointmentType}</p>
             </div>
