e992c5d605a718cd750058280f242852c56f45b3
Emmanuel Ofori <slxmtube@gmail.com>
2023-04-27 01:58:16 +0000

    Update ExploreHospitals.jsx

--- src/components/ExplorePage/ExploreHospitals.jsx
+++ src/components/ExplorePage/ExploreHospitals.jsx
@@ -1,7 +1,6 @@
 import { MedicalCentreData } from '@/data/medicalCentres';
 import ExploreStyles from '@/styles/ExplorePage.module.css';
 import MedicalCentreCard from '../Cards/MedicalCentreCard';
-import { MedicalCentreData } from '@/data/medicalCentres';
 import { useContext } from 'react';
 import { filterContext } from '@/Context/filterContext';
 
